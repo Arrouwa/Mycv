25417187cfc0dfb14ef1e2fcaddb86697062be24
Adam Abakar Hassan <99681784+Arrouwa@users.noreply.github.com>
2022-10-24 16:56:05 +0530

    Add files via upload

---
+++ user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Who are you?"
+read name
+echo "Hello, $name."
+echo "What you are doing?"
+read action
+echo "You are ${action}ing."
+echo "What you liked the most?"
+read action
+echo "I like  ${action}ing."
